875ebf829e8ca81c61301a9bdd67d016a3a1906a
정도한 <dhmilktea@cnspartner.com>
2025-08-12 22:11:51 +0900

    create api

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,6 +14,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("!!!!");
 		System.out.println("!!!!3");
+		System.out.println("!!!!2");
 	}
 
 }
